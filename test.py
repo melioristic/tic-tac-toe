f36bed01050da71998899bacfcf97962075fec0e
melioristic <itsmohitanand@gmail.com>
2022-11-18 22:25:29 +0100

    added basic colors

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
 from ttt.board import TTTBoard
+board = TTTBoard()
 
-print("Let us play Tic Tac Toe!")
-print("Enter the board state : ")
-index_list = input().split(",")
-
-state = [[] for _ in range(9)]
-
-for index in index_list:
-    state[int(index)-1] = [1]
-
-# state = [[], [], [],[0], [], [1],[], [0], []]
-
-board = TTTBoard(state)
-
-board.show_board()
--- ttt/board.py
+++ ttt/board.py
@@ -14,6 +14,9 @@
 # __status__ = Development
 # ==========================================================
 
+from colorama import Fore, Back, Style
+
+
 class TTTBoard(object):
     def __init__(self, state = None):
         """Main board of Tic Tac Toe
@@ -21,10 +24,29 @@ class TTTBoard(object):
         Args:
             state (Int, optional): 0 or 1 for O and X. Defaults to None.
         """
+        print(Style.BRIGHT, Fore.GREEN, "=======================", Style.RESET_ALL)
+        print(Style.BRIGHT, Fore.GREEN, "Welcome to Tic Tac Toe!", Style.RESET_ALL)
+        print(Style.BRIGHT, Fore.GREEN, "=======================", Style.RESET_ALL)
+
         if state != None:
             self.state = state
         else:
             self.state = [[] for i in range(9)]
+
+        self.player = 0
+        self.n_moves = 0
+
+        while self.n_moves<9:
+            self.move(self.get_move())
+            self.show_board()
+            if self.check_win():
+                if self.player == 0:
+                    col = Fore.YELLOW
+                else:
+                    col = Fore.CYAN
+                print(col, f"Player {self.player} has won!", Style.RESET_ALL)
+                break
+            self.change_player()
 
     def show_board(self):
         """
@@ -34,7 +56,7 @@ class TTTBoard(object):
             start = 3*i
             print(f"{self._convert01OX(self.state[start])} | {self._convert01OX(self.state[start+1])} | {self._convert01OX(self.state[start+2])}")              
             if i<2:
-                print("-------------")
+                print("---------------")
 
     def _convert01OX(self, element):
         if element==[0]:
@@ -45,3 +67,70 @@ class TTTBoard(object):
             element = "[ ]"
 
         return element
+
+    def move(self, position):
+        """
+        Move of the player
+
+        Args:
+            player (Int): 0 or 1 for O and X
+            position (Int): Position of the move
+        """
+        position = (position[0]-1)*3 + position[1]-1
+        if self.state[position] != []:
+            print(Fore.RED, "Invalid move!")
+            print(Style.RESET_ALL)
+            return self.move(self.get_move())
+        self.state[position] = [self.player]
+        self.n_moves += 1
+        
+
+    def change_player(self):
+        """
+        Change the player
+        """
+        if self.player == 0:
+            self.player = 1
+        else:
+            self.player = 0
+
+    def get_move(self):
+        """
+        Get the move from the player
+
+        Returns:
+            Int: Position of the move
+        """
+        if self.player == 0:
+            col = Fore.YELLOW
+        else:
+            col = Fore.CYAN
+        print(col, f"Player {self.player} move: ", Style.RESET_ALL)
+        position = input().split(",")
+        position = (int(position[0]), int(position[1]))
+        return position
+
+    def check_win(self):
+        """
+        Check if the player has won
+
+        Returns:
+            Bool: True if the player has won
+        """
+        for i in range(3):
+            if self.state[3*i] != [] and self.state[3*i] == self.state[3*i+1] and self.state[3*i] == self.state[3*i+2]:
+                return True
+            if self.state[i]!= [] and self.state[i] == self.state[i+3] and self.state[i] == self.state[i+6]:
+                return True
+        if self.state[0] != [] and self.state[0]==self.state[4]==self.state[8]:
+            return True
+        if self.state[2] != [] and self.state[2]==self.state[4]==self.state[6]:
+            return True
+
+        return False
+
+
+# print(Fore.RED + 'some red text')
+# print(Back.GREEN + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
