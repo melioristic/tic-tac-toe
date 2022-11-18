9c3225675392ed575f4a9f7d6e109e5fc27dbe0a
melioristic <itsmohitanand@gmail.com>
2022-11-18 21:23:42 +0100

    enhanced test.py

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from ttt.board import TTTBoard
 
-state = [[], [], [],[0], [], [1],[], [0], []]
+print("Let us play Tic Tac Toe!")
+print("Enter the board state : ")
+index_list = input().split(",")
+
+state = [[] for _ in range(9)]
+
+for index in index_list:
+    state[int(index)-1] = [1]
+
+# state = [[], [], [],[0], [], [1],[], [0], []]
 
 board = TTTBoard(state)
 
